4bb4593f819905d1d9d57a37faef7513e8aabb32
unknown <danilo@casadosaber.com.br>
2020-10-20 15:24:51 -0300

    Buil 20-10

---
+++ precache-manifest.447a6241a6589fca1e95afab371f4361.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55306d358d61ce25f4b1317ed6723e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "050032d27f7b58c0d0a1",
+    "url": "/static/css/main.6fbee537.chunk.css"
+  },
+  {
+    "revision": "7b576bb9623528d93afe",
+    "url": "/static/js/2.100977c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.100977c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050032d27f7b58c0d0a1",
+    "url": "/static/js/main.0ba2a10e.chunk.js"
+  },
+  {
+    "revision": "95761db79cf9b14567a2",
+    "url": "/static/js/runtime-main.e74a56b2.js"
+  },
+  {
+    "revision": "80601a7ba24fae4b16452528e4add22a",
+    "url": "/static/media/ICONE_infinito_-03.80601a7b.png"
+  },
+  {
+    "revision": "9e176a1a86fb703b6e7ef9338c53b2cc",
+    "url": "/static/media/Mockup2@2x.9e176a1a.png"
+  },
+  {
+    "revision": "f84ea005889d498cdc9b0360eaa56d0a",
+    "url": "/static/media/brainIcon.f84ea005.svg"
+  },
+  {
+    "revision": "2b33f925aa4a386ffac9fc7cb2cb7e98",
+    "url": "/static/media/checkIcon.2b33f925.svg"
+  },
+  {
+    "revision": "d03aad528281caf7c6a0b9c1c96889e8",
+    "url": "/static/media/cienceIcon.d03aad52.svg"
+  },
+  {
+    "revision": "19e0af397f43fe5b0990bffbbc5a94a1",
+    "url": "/static/media/claudia-feitosa.19e0af39.png"
+  },
+  {
+    "revision": "d1d57e8e4668f974ba5d12897d8732ab",
+    "url": "/static/media/clovis-barros.d1d57e8e.jpg"
+  },
+  {
+    "revision": "26a44f326b5f7880bfc157fa4d11c540",
+    "url": "/static/media/greateQuestionsIcon.26a44f32.svg"
+  },
+  {
+    "revision": "b284882d5002d52364a2cbcb66af2fad",
+    "url": "/static/media/historyIcon.b284882d.svg"
+  },
+  {
+    "revision": "04c319b07134d1cf2e02a946c4ce126a",
+    "url": "/static/media/literatureIcon.04c319b0.svg"
+  },
+  {
+    "revision": "8a95d619d8f15336473b7ee3dcfc3df2",
+    "url": "/static/media/luiz-hanss.8a95d619.jpg"
+  },
+  {
+    "revision": "7197e7a64ec04b0f340dad2d4f5f1ae2",
+    "url": "/static/media/maria-homem-min.7197e7a6.jpg"
+  },
+  {
+    "revision": "6f050464b61457e90a491d0c887a304d",
+    "url": "/static/media/mockup.6f043d9f@2x.6f050464.png"
+  },
+  {
+    "revision": "9b00c19c92bb8abdd23a40281c62bc13",
+    "url": "/static/media/movieIcon.9b00c19c.svg"
+  },
+  {
+    "revision": "183ff691ea9d4470aa3fd3695da525cf",
+    "url": "/static/media/paintingIcon.183ff691.svg"
+  },
+  {
+    "revision": "1e3521dad169b3a481200a21b7cea2d5",
+    "url": "/static/media/philosophyIcon.1e3521da.svg"
+  },
+  {
+    "revision": "50f933f6ed5c964941e3bd7217ac2bf8",
+    "url": "/static/media/religionIcon.50f933f6.svg"
+  },
+  {
+    "revision": "9a9b083e5f7ce4ac960e38bea12d1a0e",
+    "url": "/static/media/theaterIcon.9a9b083e.svg"
+  }
+]);
